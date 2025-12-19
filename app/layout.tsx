7eed7359101759dcb8aefcfab3ab9d19f0e1c8a6
hawai1401 <iyed.bz@gmail.com>
2025-12-19 15:14:06 +0000

    Ajout du Toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Header/Nav";
 import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/sonner";
 
 export const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -66,6 +67,7 @@ export default function RootLayout({
         <Nav />
         {children}
         <Footer />
+        <Toaster position="top-center" visibleToasts={3} />
       </body>
     </html>
   );
