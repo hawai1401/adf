cd879a569cac02426f1c7a5df43571ec8574673c
hawai1401 <hawai1401@ik.me>
2025-12-21 18:15:46 +0100

    Prise en charge du markdown sur la page des serveurs

--- components/serveurs/Serveur.tsx
+++ components/serveurs/Serveur.tsx
@@ -1,5 +1,7 @@
+import parseInline from "@/lib/parseInline";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { ReactNode } from "react";
 
 export default function Serveur({
   logo,
@@ -23,6 +25,53 @@ export default function Serveur({
     GRAPHISME: "primary",
     COMMUNAUTAIRE: "info",
   };
+
+  const desc: ReactNode = description.split("\n").map((s, i) => {
+    // Titres
+    if (s.startsWith("### "))
+      return (
+        <div className="text-lg font-semibold" key={i}>
+          {parseInline(s.slice(4))}
+        </div>
+      );
+
+    if (s.startsWith("## "))
+      return (
+        <div className="text-xl font-semibold" key={i}>
+          {parseInline(s.slice(3))}
+        </div>
+      );
+
+    if (s.startsWith("# "))
+      return (
+        <div className="text-2xl font-bold" key={i}>
+          {parseInline(s.slice(2))}
+        </div>
+      );
+
+    // Blockquote >
+    if (s.startsWith("> "))
+      return (
+        <div key={i} className="border-l-4 border-gray-500 pl-3 opacity-90">
+          {parseInline(s.slice(2))}
+        </div>
+      );
+
+    // Listes
+    if (s.startsWith("- ") || s.startsWith("* "))
+      return (
+        <div key={i} className="flex gap-2 pl-2">
+          <span>•</span>
+          <span>{parseInline(s.slice(2))}</span>
+        </div>
+      );
+
+    // Ligne vide
+    if (s.trim() === "") return <br key={i} />;
+
+    // Texte normal
+    return <div key={i}>{parseInline(s)}</div>;
+  });
   return (
     <div className="bg-base-300 p-4 rounded-box flex flex-col w-full sm:w-[calc(100%/2-2%)] lg:w-[calc(100%/3-2%)] gap-4">
       <div className="flex justify-center items-center gap-4 mb-0.5">
@@ -45,8 +94,8 @@ export default function Serveur({
         </div>
       </div>
       <hr className="w-full h-2 border-accent" />
-      <p className="text-justify mb-0.5">{description}</p>
-      {badges && (
+      <p className="text-justify mb-0.5 flex flex-col">{desc}</p>
+      {badges && badges.length > 0 && (
         <>
           <hr className="w-full h-2 border-accent" />
           <div className="flex flex-wrap items-center justify-center">
